41a0ffe241701d9e35cef45436fa2fb4c1888577
valeolmos <valentinaolmos2003@gmail.com>
2023-05-12 13:49:09 -0300

    mister weenie

--- PRYOlmosControlesBasicos/Form1.Designer.cs
+++ PRYOlmosControlesBasicos/Form1.Designer.cs
@@ -28,13 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblEjeY = new System.Windows.Forms.Label();
+            this.lblEjeX = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblEjeY
+            // 
+            this.lblEjeY.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblEjeY.AutoSize = true;
+            this.lblEjeY.Location = new System.Drawing.Point(393, 10);
+            this.lblEjeY.Name = "lblEjeY";
+            this.lblEjeY.Size = new System.Drawing.Size(14, 13);
+            this.lblEjeY.TabIndex = 0;
+            this.lblEjeY.Text = "Y";
+            this.lblEjeY.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label2_MouseMove);
+            // 
+            // lblEjeX
+            // 
+            this.lblEjeX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblEjeX.AutoSize = true;
+            this.lblEjeX.Location = new System.Drawing.Point(393, 36);
+            this.lblEjeX.Name = "lblEjeX";
+            this.lblEjeX.Size = new System.Drawing.Size(14, 13);
+            this.lblEjeX.TabIndex = 1;
+            this.lblEjeX.Text = "X";
+            this.lblEjeX.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label2_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(419, 450);
+            this.Controls.Add(this.lblEjeX);
+            this.Controls.Add(this.lblEjeY);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Titulo de la ventana";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblEjeY;
+        private System.Windows.Forms.Label lblEjeX;
     }
 }
 
--- PRYOlmosControlesBasicos/Form1.cs
+++ PRYOlmosControlesBasicos/Form1.cs
@@ -16,5 +16,26 @@ namespace PRYOlmosControlesBasicos
         {
             InitializeComponent();
         }
+
+        private void label2_MouseMove(object sender, MouseEventArgs e)
+        {
+              
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblEjeX.Text = e.X.ToString();
+            lblEjeY.Text = e.Y.ToString();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Bienvenido");
+        }
     }
 }
