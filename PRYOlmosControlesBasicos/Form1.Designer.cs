86fc30046cc00dbbf11c61d9e4e110d465368f3b
valentina olmos <valentinaolmos2003@gmail.com>
2023-06-07 15:42:54 -0300

    762023

--- PRYOlmosControlesBasicos/Form1.Designer.cs
+++ PRYOlmosControlesBasicos/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PRYOlmosControlesBasicos
 {
-    partial class Form1
+    partial class frmEjemplo
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,9 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEjemplo));
             this.lblEjeY = new System.Windows.Forms.Label();
             this.lblEjeX = new System.Windows.Forms.Label();
+            this.chkRojo = new System.Windows.Forms.CheckBox();
+            this.chkAzul = new System.Windows.Forms.CheckBox();
+            this.chkAmarillo = new System.Windows.Forms.CheckBox();
+            this.lblEjemplo = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblEjeY
@@ -55,16 +59,64 @@
             this.lblEjeX.Text = "X";
             this.lblEjeX.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label2_MouseMove);
             // 
-            // Form1
+            // chkRojo
+            // 
+            this.chkRojo.AutoSize = true;
+            this.chkRojo.Location = new System.Drawing.Point(36, 77);
+            this.chkRojo.Name = "chkRojo";
+            this.chkRojo.Size = new System.Drawing.Size(48, 17);
+            this.chkRojo.TabIndex = 2;
+            this.chkRojo.Text = "Rojo";
+            this.chkRojo.UseVisualStyleBackColor = true;
+            this.chkRojo.CheckedChanged += new System.EventHandler(this.chkRojo_CheckedChanged);
+            // 
+            // chkAzul
+            // 
+            this.chkAzul.AutoSize = true;
+            this.chkAzul.Location = new System.Drawing.Point(36, 101);
+            this.chkAzul.Name = "chkAzul";
+            this.chkAzul.Size = new System.Drawing.Size(46, 17);
+            this.chkAzul.TabIndex = 3;
+            this.chkAzul.Text = "Azul";
+            this.chkAzul.UseVisualStyleBackColor = true;
+            this.chkAzul.CheckedChanged += new System.EventHandler(this.chkAzul_CheckedChanged);
+            // 
+            // chkAmarillo
+            // 
+            this.chkAmarillo.AutoSize = true;
+            this.chkAmarillo.Location = new System.Drawing.Point(36, 125);
+            this.chkAmarillo.Name = "chkAmarillo";
+            this.chkAmarillo.Size = new System.Drawing.Size(62, 17);
+            this.chkAmarillo.TabIndex = 4;
+            this.chkAmarillo.Text = "Amarillo";
+            this.chkAmarillo.UseVisualStyleBackColor = true;
+            this.chkAmarillo.CheckedChanged += new System.EventHandler(this.chkAmarillo_CheckedChanged);
+            // 
+            // lblEjemplo
+            // 
+            this.lblEjemplo.AutoSize = true;
+            this.lblEjemplo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblEjemplo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEjemplo.Location = new System.Drawing.Point(36, 174);
+            this.lblEjemplo.Name = "lblEjemplo";
+            this.lblEjemplo.Size = new System.Drawing.Size(181, 27);
+            this.lblEjemplo.TabIndex = 5;
+            this.lblEjemplo.Text = "Texto de Ejemplo";
+            // 
+            // frmEjemplo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(419, 450);
+            this.Controls.Add(this.lblEjemplo);
+            this.Controls.Add(this.chkAmarillo);
+            this.Controls.Add(this.chkAzul);
+            this.Controls.Add(this.chkRojo);
             this.Controls.Add(this.lblEjeX);
             this.Controls.Add(this.lblEjeY);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form1";
-            this.Text = "Titulo de la ventana";
+            this.Name = "frmEjemplo";
+            this.Text = "Prueva de Controles Comunes";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
@@ -77,6 +129,10 @@
 
         private System.Windows.Forms.Label lblEjeY;
         private System.Windows.Forms.Label lblEjeX;
+        private System.Windows.Forms.CheckBox chkRojo;
+        private System.Windows.Forms.CheckBox chkAzul;
+        private System.Windows.Forms.CheckBox chkAmarillo;
+        private System.Windows.Forms.Label lblEjemplo;
     }
 }
 
--- PRYOlmosControlesBasicos/Form1.cs
+++ PRYOlmosControlesBasicos/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,9 @@ using System.Windows.Forms;
 
 namespace PRYOlmosControlesBasicos
 {
-    public partial class Form1 : Form
+    public partial class frmEjemplo : Form
     {
-        public Form1()
+        public frmEjemplo()
         {
             InitializeComponent();
         }
@@ -36,6 +37,44 @@ namespace PRYOlmosControlesBasicos
         private void Form1_MouseClick(object sender, MouseEventArgs e)
         {
            
+        }
+
+        private void chkRojo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkRojo.Checked == true);
+           
+                lblEjemplo.ForeColor = Color.Red;
+               if (chkAzul.Checked == true)
+            {
+                lblEjemplo.ForeColor = Color.Violet;
+            }
+            if (chkAmarillo.Checked == true)
+            {
+                lblEjemplo.ForeColor = Color.Orange;
+                    }
+
+             lblEjemplo.ForeColor = Color.Black;
+
+        }
+
+        private void chkAzul_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (chkAzul.Checked == true) ;
+
+            lblEjemplo.ForeColor = Color.Blue;
+
+            lblEjemplo.ForeColor = Color.Black;
+        }
+
+        private void chkAmarillo_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (chkAmarillo.Checked == true) ;
+
+            lblEjemplo.ForeColor = Color.Yellow;
+
+            lblEjemplo.ForeColor = Color.Black;
         }
     }
 }
